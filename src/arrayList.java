7a6c9b3b35a505ec5f0efd97d4e32d150f791d3c
jaga2906 <jagadeeshkumarmv@hotmail.com>
2019-01-30 08:10:40 +0530

    test case modified

--- src/arrayList.java
+++ src/arrayList.java
@@ -2,6 +2,7 @@
 public class arrayList {
 	public static void main(String[] arguments) {
 	    System.out.println("Let's do something using Java technology.");
+	    System.out.println("Let's do something using Selenium technology.");
 	  }
 	}
 
